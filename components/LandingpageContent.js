ef8196bc6f32c1b3205ffbbd0928a9d366b7b373
LohremYpsum <102668432+LohremYpsum@users.noreply.github.com>
2023-07-12 00:46:30 +0200

    Update LandingpageContent.js

--- components/LandingpageContent.js
+++ components/LandingpageContent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Logo from './Logo';
+import Image from 'next/image';
 
 const LandingpageContent = () => {
   return (
@@ -22,18 +23,34 @@ const LandingpageContent = () => {
         </div>
 
         <div id='howitworks' className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-            <img className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
-            <img className="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image" />
+        <Image
+                className="w-full dark:hidden"
+                width={100}
+                height={200}
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+                alt="dashboard image"
+              />
+
+<Image
+                className="w-full hidden dark:block"
+                width={100}
+                height={200}
+                src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg"
+                alt="dashboard image"
+              />
+
+          
+          
             <div className="mt-4 md:mt-0">
                 <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">How it works</h2>
                 <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-                To access the page, navigate to the "Encrypt Message" option in the navigation menu. 
+                To access the page, navigate to the Encrypt Message option in the navigation menu. 
                 Fill in your first name, last name, and email address, then generate a token. Ensure that you also include the message you wish to have encrypted. 
                 It is important to remember your credentials and the generated token and share them with the authorized person responsible for decrypting the message.
                   <br />
                   <br />
-                  Next, the intended recipient should locate and click on the "Decrypt Message" option in the header. 
-                  They will need to enter the provided credentials. It is important to remember to select the "Decrypt Message" button to initiate the decryption process and access the message. 
+                  Next, the intended recipient should locate and click on the Decrypt Message option in the header. 
+                  They will need to enter the provided credentials. It is important to remember to select the Decrypt Message button to initiate the decryption process and access the message. 
                   
                   <br></br> <br></br>Have fun!
                   </p>
